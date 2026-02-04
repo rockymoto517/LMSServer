f1fb4f647ee4f1bc9d1a380ca31f8c04a7e2be1a
hzllyy <hzlly.zheng@gmail.com>
2026-02-03 17:34:20 -0800

    done

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
-  res.json({ status: 'ok', version: 'v2' });
+  res.json({ status: 'ok' });
 });
 
 app.get('/api/hello', (req, res) => {
